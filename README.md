6b47571f1c20dcd76dcaff1e3b61ebb2b3d97a2c
Vaibhavkulshrestha12 <vaibhavkulshrestha55@gmail.com>
2024-11-04 01:46:28 +0530

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Streaming--POC
+A template for any streaming app that will be developed in any further future 
---
+++ index.js
@@ -0,0 +1,65 @@
+import express from "express"
+import cors from "cors"
+import multer from "multer"
+import {v4 as uuidv4} from "uuid"
+import path from "path"
+
+const app = express()
+
+//multer MW
+
+const storage = multer.diskStorage({
+    destination: function(req, file , cb){
+    cb(null, "./uploads")
+    },
+    filename: function(req, file, cb){
+        cb(null,file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
+    }
+})
+
+//mult config
+
+const upload = multer({
+    storage: storage,
+})
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000","http://localhost:5173"],
+        credentials: true,
+    })
+)
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+  })
+
+
+
+ app.use(express.json())
+ app.use(express.urlencoded({extended: true})) 
+ app.use("/uploads", express.static("uploads"))
+
+
+
+app.get("/", function(req, res){
+    res.json({message: "Hello Protagnist"})
+})
+
+app.post("/upload", upload.single('file'),function(req,res){
+    const lessonId = uuidv4()
+    const videoPath = req.file.path
+    const outputPath = `./uploads/courses/${lessonId}`
+    const hlsPath = `${outputPath}/index.m3u8`
+
+})
+
+app.listen(8000,function(){
+    console.log("Server is running")
+})
+
